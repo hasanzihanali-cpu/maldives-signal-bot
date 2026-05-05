223c3540474584e669e4d86ebaa11df1112ba19a
hasanzihanali-cpu <hasanzihanali@gmail.com>
2026-05-05 15:05:32 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,9 @@ TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 RSS_FEEDS = [
     "https://edition.mv/rss",
     "https://raajje.mv/feed",
+    "https://sun.mv/feed",
+    "https://avas.mv/feed",
+    "https://mihaaru.com/feed",
 ]
 
-SCORE_THRESHOLD = 40
+SCORE_THRESHOLD = 50
